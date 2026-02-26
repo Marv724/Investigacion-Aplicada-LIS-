6219fbe07a9a79e7b95f62e661b5467b27e520fc
JavierCastillo <CH230404@alumno.udb.edu.sv>
2026-02-25 18:50:37 -0600

    Funcionamiento de botón actualizar

--- Backend.php
+++ Backend.php
@@ -30,9 +30,53 @@ if (isset($_POST['crear'])) {
         "requerimientos" => $_POST['requerimientos'],
         "documento" => $archivo
     ];
+
+    header("Location: Principal.php");
+    exit();
 }
 
 /* ELIMINAR */
 if (isset($_GET['eliminar'])) {
-    unset($_SESSION['reservas'][$_GET['eliminar']]);
+    $indice = $_GET['eliminar'];
+
+    if(isset($_SESSION['reservas'][$indice])) {
+        unset($_SESSION['reservas'][$indice]);
+        $_SESSION['reservas'] = array_values($_SESSION['reservas']);
+    }
+    header("Location: Principal.php");
+    exit();
+}
+
+/* EDITAR */
+if (isset($_POST['editar'])) {
+    $indice = $_POST['editar'];
+
+    if(isset($_SESSION['reservas'][$indice])) {
+        $archivo = $_SESSION['reservas'][$indice]['documento'];
+
+        if (!empty($_FILES['documento']['name'])) {
+            if (!is_dir("uploads")) {
+                mkdir("uploads", 0777, true);
+            }
+
+            $archivo = time() . "_" . $_FILES['documento']['name'];
+            move_uploaded_file($_FILES['documento']['tmp_name'], "uploads/" . $archivo);
+        }
+
+        $_SESSION['reservas'][$indice] = [
+            "nombre" => $_POST['nombre'],
+            "direccion" => $_POST['direccion'],
+            "telefono" => $_POST['telefono'],
+            "evento" => $_POST['evento'],
+            "personas" => $_POST['personas'],
+            "fecha" => $_POST['fecha'],
+            "inicio" => $_POST['inicio'],
+            "fin" => $_POST['fin'],
+            "pago" => $_POST['pago'],
+            "requerimientos" => $_POST['requerimientos'],
+            "documento" => $archivo
+        ];
+    }
+    echo "<script>window.location='Principal.php';</script>";
+    exit();
 }
